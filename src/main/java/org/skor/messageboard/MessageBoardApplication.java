fa2f216fe2f5aa33583631f2136a7a06c8e29450
Sergey Korotkov <serge.korotkov@gmail.com>
2017-08-23 01:30:12 +0700

    initial add

---
+++ src/main/java/org/skor/messageboard/MessageBoardApplication.java
@@ -0,0 +1,14 @@
+package org.skor.messageboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+public class MessageBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageBoardApplication.class, args);
+    }
+}
